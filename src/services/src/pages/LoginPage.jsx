17dc7a46ba656c732b8dc915f329691c3beb86cb
ayushmadeshiya <ayushkumar15.03.2005@gmail.com>
2025-07-03 15:49:49 +0530

    Create LoginPage.jsx

---
+++ src/services/src/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import LoginForm from '../components/LoginForm';
+
+export default function LoginPage() {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold">Login</h1>
+      <LoginForm />
+    </div>
+  );
+}
