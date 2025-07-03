8697376e54406a64354923bf7521ccb95e66fd68
ayushmadeshiya <ayushkumar15.03.2005@gmail.com>
2025-07-03 15:50:24 +0530

    Create RegisterPage.jsx

---
+++ src/services/src/pages/src/pages/RegisterPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import RegisterForm from '../components/RegisterForm';
+
+export default function RegisterPage() {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold">Register</h1>
+      <RegisterForm />
+    </div>
+  );
+}
