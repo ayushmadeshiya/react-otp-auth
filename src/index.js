48743494dac43ffcee5207a89a2c842dea44270f
ayushmadeshiya <ayushkumar15.03.2005@gmail.com>
2025-07-03 16:33:29 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { BrowserRouter } from 'react-router-dom';
 
 const queryClient = new QueryClient();
 
